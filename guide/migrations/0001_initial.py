33bb7446404b8c538a52dbb9925b85049c2df4b9
haradhansharma <haradhan.sharma@gmail.com>
2022-06-04 16:29:32 +0600

    upgradsed

--- guide/migrations/0001_initial.py
+++ guide/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=252)),
                 ('anchor', models.CharField(max_length=252)),
-                # ('content', tinymce.models.HTMLField()),
+                ('content', models.CharField(max_length=252)),
             ],
         ),
     ]
