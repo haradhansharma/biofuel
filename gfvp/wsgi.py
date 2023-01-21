ac23b5492db8f80d7799f34a9cb21499177411f1
haradhansharma <haradhan.sharma@gmail.com>
2023-01-21 15:32:04 +0600

    AddingSOPackage

--- gfvp/wsgi.py
+++ gfvp/wsgi.py
@@ -1,26 +1,13 @@
-"""
-WSGI config for gfvp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 
 import os
 import sys
 
-import logging
-log =  logging.getLogger('log')
-log.info(f'os name is  {os.name} ')   
 if os.name != 'nt':
     path = '/home/gfvpcom/public_html'
     python_path = '/home/gfvpcom/gfvpenv/lib/python3.9/site-packages'
     sys.path.append(path)
     sys.path.append(python_path)
-
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gfvp.settings')
+    
 os.environ['DJANGO_SETTINGS_MODULE'] = 'gfvp.settings'
 
 from django.core.wsgi import get_wsgi_application
