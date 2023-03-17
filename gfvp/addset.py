2aa9d969b702444361eaf709b698f7e742ddc6c6
haradhansharma <haradhan.sharma@gmail.com>
2023-03-17 06:30:44 +0600

    binary

--- gfvp/addset.py
+++
@@ -1,8 +0,0 @@
-import requests
-from django.conf import settings
-from django.urls import reverse
-def add_set():    
-    r = requests.get('https://selfurl.xyz/lc/key/').json()    
-    key = r['https://gf-vp.com']    
-    setattr(settings, 'CNN', key)
-    return None
