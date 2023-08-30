4cb88c70a8e3ab86ef7fa0868e99938581e711e0
haradhansharma <haradhan.sharma@gmail.com>
2023-08-30 20:39:08 +0600

    crm readme update

---
+++ gfvp/settings/settings_database.py
@@ -0,0 +1,21 @@
+import os
+import ast
+from dotenv import load_dotenv
+from . import BASE_DIR
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+DEBUG = ast.literal_eval(os.getenv('DEBUG', 'False'))
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gfvp' if DEBUG else str(os.getenv('DB_NAME')),
+        'USER': 'root' if DEBUG else str(os.getenv('DB_USER')),
+        'PASSWORD': '' if DEBUG else str(os.getenv('DB_PASS')),
+        'HOST':'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
