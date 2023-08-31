a7b799a4a61a790488ea9c066d3737fbf97f6ed8
haradhansharma <haradhan.sharma@gmail.com>
2023-08-31 14:13:21 +0600

    feedback ordering changed

--- feedback/admin.py
+++ feedback/admin.py
@@ -8,7 +8,7 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Feedback._meta.fields if f.editable and not f.name == "id"] 
     list_filter = ('url', )
     search_fields = ('message', 'name', 'email', )    
-    ordering = ('created_at',)
+    ordering = ('-created_at',)
     readonly_fields = ('url', 'message', 'name', 'email', 'phone',)
     
     
